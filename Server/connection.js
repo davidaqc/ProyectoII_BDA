7ad29373011d87eb93e2b775001134776d176d2f
Cristian Marin <cristian-44@hotmail.es>
2021-11-04 00:19:52 -0600

    Delete Server directory

--- Server/connection.js
+++
@@ -1,5 +0,0 @@
-var neo4j = require("neo4j-driver");
-
-//Conexion con la base de datos
-var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "pass"));
-var session = driver.session();
